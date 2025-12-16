ca484f3e0875402a1c5a82529dc3cb8a4543bad7
d2h577yf <why20051028XY@icloud.com>
2025-12-16 21:50:36 +0800

    2025Dec16

--- MPL/cov_correct.py
+++ MPL/cov_correct.py
@@ -4,37 +4,38 @@ from d2l import torch as d2l
 
 dropout1,dropout2 = 0.2,0.5
 
-def dropout_layer(X, dropout):
-    assert 0 <= dropout <= 1
-    # 在本情况中，所有元素都被丢弃
-    if dropout == 1:
-        return torch.zeros_like(X)
-    # 在本情况中，所有元素都被保留
-    if dropout == 0:
-        return X
-    mask = (torch.rand(X.shape) > dropout).float()
-    return mask * X / (1.0 - dropout)
 
-class Net(nn.Module):
-    def __init__(self, num_inputs, num_outputs, num_hiddens1, num_hiddens2,
-                 is_training = True):
-        super(Net, self).__init__()
-        self.num_inputs = num_inputs
-        self.training = is_training
-        self.lin1 = nn.Linear(num_inputs, num_hiddens1)
-        self.lin2 = nn.Linear(num_hiddens1, num_hiddens2)
-        self.lin3 = nn.Linear(num_hiddens2, num_outputs)
-        self.relu = nn.ReLU()
+# def dropout_layer(X, dropout):
+#     assert 0 <= dropout <= 1
+#     # 在本情况中，所有元素都被丢弃
+#     if dropout == 1:
+#         return torch.zeros_like(X)
+#     # 在本情况中，所有元素都被保留
+#     if dropout == 0:
+#         return X
+#     mask = (torch.rand(X.shape) > dropout).float()
+#     return mask * X / (1.0 - dropout)
 
-    def forward(self, X):
-        H1 = self.relu(self.lin1(X.reshape((-1, self.num_inputs))))
-        if self.training:
-            H1 = dropout_layer(H1, dropout1)
-        H2 = self.relu(self.lin2(H1))
-        if self.training:
-            H2 = dropout_layer(H2, dropout2)
-        out = self.lin3(H2)
-        return out
+# class Net(nn.Module):
+#     def __init__(self, num_inputs, num_outputs, num_hiddens1, num_hiddens2,
+#                  is_training = True):
+#         super(Net, self).__init__()
+#         self.num_inputs = num_inputs
+#         self.training = is_training
+#         self.lin1 = nn.Linear(num_inputs, num_hiddens1)
+#         self.lin2 = nn.Linear(num_hiddens1, num_hiddens2)
+#         self.lin3 = nn.Linear(num_hiddens2, num_outputs)
+#         self.relu = nn.ReLU()
+#
+#     def forward(self, X):
+#         H1 = self.relu(self.lin1(X.reshape((-1, self.num_inputs))))
+#         if self.training:
+#             H1 = dropout_layer(H1, dropout1)
+#         H2 = self.relu(self.lin2(H1))
+#         if self.training:
+#             H2 = dropout_layer(H2, dropout2)
+#         out = self.lin3(H2)
+#         return out
 
 if __name__ == '__main__':
     num_inputs, num_outputs, num_hiddens1, num_hiddens2 = 784, 10, 256, 256
@@ -51,7 +52,6 @@ if __name__ == '__main__':
             nn.Linear(num_hiddens1,num_hiddens2),
             nn.ReLU(),
             nn.Dropout(dropout2),
-
             nn.Linear(num_hiddens2,num_outputs)
     )
 
