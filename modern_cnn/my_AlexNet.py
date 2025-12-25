11868512d2fb7a49691e48ac5e3a171bb5ba0e84
d2h577yf <why20051028XY@icloud.com>
2025-12-25 12:11:18 +0800

    make train and predict function as a lib fix

--- modern_cnn/my_AlexNet.py
+++ modern_cnn/my_AlexNet.py
@@ -1,18 +1,16 @@
-import torch
-import torch.nn as nn
-
+# Library import
 import time
 import matplotlib.pyplot as plt
 import torch
 import torchvision
 import torchvision.transforms as transforms
 from torch import nn
-from torch import optim
 from torch.utils import data
-from tqdm import tqdm
+import train_predict as tp
 
 plt.rcParams['font.sans-serif'] = ['Arial Unicode MS', 'Songti SC', 'STHeiti']
 plt.rcParams['axes.unicode_minus'] = False
+
 
 def main():
     pass
